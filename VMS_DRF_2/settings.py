fe5fdee6bfd9276d8a31d1303c2f149c86ee79a0
MrTuzzO <khirulislam5750@gmail.com>
2024-12-23 00:38:30 +0600

    host updated

--- VMS_DRF_2/settings.py
+++ VMS_DRF_2/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-kh2f6$q=+7tay@yrse6o9@$^wprsdqospr01m&0dim)_=_$vhz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app"]
 
 
 # Application definition
